4062f59c667266095bcb7c8bf14a9581ad8750bb
samir <me@samirpatel.me>
2024-07-23 10:49:33 -0500

    map slices and multiple return values

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,8 +1,40 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
 
-func Hello(name string) string {
-	message := fmt.Sprintf("Hi, %v welcome!", name)
-	return message
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name not allowed")
+	}
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+func Hellos(names []string) (map[string]string, error) {
+	// a map to associate names with messages
+	messages := make(map[string]string)
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		//associate the retrieved name with the map
+		messages[name] = message
+	}
+	return messages, nil
+}
+
+func randomFormat() string {
+	// a slice of message formats
+	formats := []string{
+		"Hi %v, welcome",
+		"good to see you %v",
+		"kiss my ass %v",
+	}
+	numFormats := len(formats)
+	return formats[rand.Intn(numFormats)]
 }
--- hello/hello.go
+++ hello/hello.go
@@ -2,12 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/samirspatel/go-examples/greetings"
 	"rsc.io/quote"
 )
 
 func main() {
+
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("samir")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(quote.Go())
-	fmt.Println(greetings.Hello("Samir"))
+	fmt.Println(message)
+
+	// pass in a slice of names
+	names := []string{"max", "sluggy", "chungus"}
+
+	//request messages
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// if no error returned, print messages
+	fmt.Println(messages)
 }
